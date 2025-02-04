8c10aaabf4350fff6872465ce59366f395671ba9
Procurio-AG <1108.anantgupta@gmail.com>
2025-02-04 22:22:05 +0530

    the player triangle has a circular hitbox and works fine with all WASD keys

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 #importing pygame open source library
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen  = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player_1 = Player(x = SCREEN_WIDTH/2,y = SCREEN_HEIGHT/2)
     while(1):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(color="black")
+        player_1.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60)/1000
+        player_1.update(dt)
 
 if __name__ == "__main__":
     main()
---
+++ player.py
@@ -0,0 +1,60 @@
+import pygame
+import circleshape as cs
+from constants import *
+
+#player class
+class Player(cs.CircleShape):
+    def __init__(self,x,y):
+        super().__init__(x,y,PLAYER_RADIUS)
+        self.rotation = 0
+
+    # in the player class
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+
+    #overriding draw
+    def draw(self,screen):
+        pygame.draw.polygon(
+                surface = screen,
+                color = "white",
+                points = self.triangle(),
+                width = 2)
+
+    #player rotation
+    def rotate(self,dt):
+        #print(f"Current rotation: {self.rotation}")  #debug line
+        self.rotation += PLAYER_TURN_SPEED*dt
+        self.rotation = self.rotation%360
+
+    #player movement
+    def move(self,dt):
+        forward = pygame.Vector2(0,1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
+    
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            # left key
+            #print("A key pressed")  #  debug line
+            self.rotate(-dt)
+        
+        if keys[pygame.K_d]:
+            # right key
+            #print("D key pressed")  # Add this debug line
+            self.rotate(dt)
+        
+        if keys[pygame.K_w]:
+            # up key
+            #print("W key pressed")  #  debug line
+            self.move(dt)
+        
+        if keys[pygame.K_s]:
+            # down key
+            #print("s key pressed")  # Add this debug line
+            self.move(-dt)
